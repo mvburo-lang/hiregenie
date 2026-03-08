40ce396eb4bd91274244b74873ec592e05fbf66a
mvburo-lang <mvburo@gmail.com>
2026-03-07 23:36:13 -0500

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -7,7 +7,6 @@ import OpenAI from "openai";
 
 const openai = new OpenAI({
   apiKey: process.env.AI_INTEGRATIONS_OPENAI_API_KEY,
-  baseURL: process.env.AI_INTEGRATIONS_OPENAI_BASE_URL,
 });
 
 export async function registerRoutes(
